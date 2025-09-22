cd97e1b7bd62e0b4f3cea1c2ce9d58f7eeaa75df
Riley {Silly} <riley@riley-vm.com>
2025-09-22 02:37:41 +0100

    RecRoomSetup.exe-Downoader

---
+++ RecRoomSetup.exe-Downoader/README.md
@@ -0,0 +1,2 @@
+# RecRoomSetup.exe-Downoader
+this is a batch script i made to download the RecRoomSetup.exe using curl idk why i made it but i did
---
+++ RecRoomSetup.exe-Downoader/RecRoomSetup.exe-Downoader.bat
@@ -0,0 +1,11 @@
+@echo off
+
+title Downloading RecRoomSetup.exe & echo Downloading RecRoomSetup.exe & curl https://cdn.rec.net/static/Self_Distribute/RecRoomSetup.exe --output RecRoomSetup.exe
+
+title Download complete opening RecRoomSetup.exe & echo Download complete opening RecRoomSetup.exe & RecRoomSetup.exe
+
+title Setup complete, deleting RecRoomSetup.exe & echo Setup complete, deleting RecRoomSetup.exe & del RecRoomSetup.exe
+
+title Command Prompt
+
+pause
